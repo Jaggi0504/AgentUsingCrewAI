841969138c197da6c9ac26ea9bfafa9a497fe410
Jagdeep Singh <singh.jagdeep0504@gmail.com>
2026-02-16 13:21:24 -0500

    updated code

--- agents.py
+++ agents.py
@@ -1,30 +1,36 @@
-from crewai import Agent
+from crewai import Agent, LLM
 from tools import yt_tool
+import os
+from dotenv import load_dotenv
 
-##Create a blog content researcher
+load_dotenv()
+print("OPENAI KEY:", os.getenv("OPENAI_API_KEY"))
 
-blog_researcher=Agent(
+llm = LLM(
+    model="groq/llama3-70b-8192",
+    api_key=os.getenv("GROQ_API_KEY"),
+)
+
+
+
+blog_researcher = Agent(
     role="Blog Researcher from YouTube videos",
     goal="Get the relevant video content for the topic {topic} from YouTube channel",
     verbose=True,
     memory=True,
-    backstory=(
-        "Expert in understanding videos in AI, Data Science, Machine Learning, and Gen AI and give suggestions"
-    ),
+    backstory="Expert in understanding AI videos and giving suggestions",
     tools=[yt_tool],
+    llm=llm,
     allow_delegation=True
 )
 
-##Create a blog writer agent with YT tool
-
-blog_writer=Agent(
+blog_writer = Agent(
     role="Blog Writer",
-    goal="Narrate compiling tech stories about the video {topic} from YouTube channel",
+    goal="Narrate compiling tech stories about the video {topic}",
     verbose=True,
     memory=True,
-    backstory=(
-        "With a flair for simplifying complex topics, you craft engaging narratives that captivate and educate, bringing new discoveries to light in an accessible manner"
-    ),
+    backstory="You simplify complex topics into engaging narratives",
     tools=[yt_tool],
+    llm=llm,
     allow_delegation=False
 )
